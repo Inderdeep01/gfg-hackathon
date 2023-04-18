8b1f3c5c025eeb9d1775bfb8a547a5466af27614
inderdeep01 <inderdeepsidhu01@gmail.com>
2023-04-19 00:33:29 +0530

    v1.0

--- routes/getTransactions.js
+++ routes/getTransactions.js
@@ -6,17 +6,22 @@ const Tx = require('../models/transactionModel')
 const protect = require('../middleware/protect')
 
 router.get('/',protect,async (req,res)=>{
-    const page = (req.query.page===NaN || req.query.page < 0 ) ? 1 : req.query.page
-    const perPage = 20
-    const id = req.user._id.toString()
-    const txs = await Tx.find({$or:[{from:req.user._id},{to:req.user._id}]}).sort({createdAt:-1})
-        .skip((page-1)*perPage)
-        .limit(perPage).populate([
-            {path:'from',select:'-password -wallet -cards -currencies -createdAt -updatedAt'},
-            {path:'to',select:'-password -wallet -cards -currencies -createdAt -updatedAt'},
-            {path:'card',select:'cardNumber expiry cvv network purpose'}
-        ])
-    return res.status(200).json(txs)
+    try {
+        const page = (req.query.page===NaN || req.query.page <= 0 ) ? 1 : req.query.page
+        const perPage = 20
+        const id = req.user._id.toString()
+        const txs = await Tx.find({$or:[{from:req.user._id},{to:req.user._id}]}).sort({createdAt:-1})
+            .skip((page-1)*perPage)
+            .limit(perPage).populate([
+                {path:'from',select:'-password -wallet -cards -currencies -createdAt -updatedAt'},
+                {path:'to',select:'-password -wallet -cards -currencies -createdAt -updatedAt'},
+                {path:'card',select:'cardNumber expiry cvv network purpose'}
+            ])
+        return res.status(200).json(txs)
+    }
+    catch{
+        return res.status(500).json({message:'Internal Server Error'})
+    }
 })
 
 router.post('/card',protect,async (req,res)=>{
